1f3dddb995434919f48f43f70399cc6f91dbc6a7
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-02-13 15:05:08 +0100

    feat(releases): initial commit

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Get container instance
+ * @return \Elgg\Di\PublicContainer
+ */
+function elgg_di() {
+	return \Elgg\Di\PublicContainer::getInstance();
+}
+
+\Elgg\Application::start();
+
+elgg_register_event_handler('cache:flush', 'system', '\Elgg\Di\PublicContainer::flush');
